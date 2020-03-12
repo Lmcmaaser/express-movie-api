7dde9e065a389ddf0b515f63a3d9a0a3d9a0efa4
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-03-11 18:32:23 -0700

    CORS policy error

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const MOVIEAPI = ('./movies-data-small.json')
-console.log(process.env.API_TOKEN)
 const app = express()
-app.use(morgan('dev'))
 
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
 // adding the validate middleware 1 time +
 // validating every request before it gets to the next handler middlewares.
 // takes 3 parameters
 //  3 param is callback function:
     // what we can call if we want to move to the next station in the factory line
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
     console.log('validate bearer token middleware')
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request'})
@@ -21,28 +24,27 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-/* const validFilters = ['genre','country', 'avg_vote']
-function handleGetFilter(req, res) {
-    res.json(validFilters)
-}
-app.get('/filter', handleGetFilter);*/
-
 app.get('/movies', function handleGetMovies(req, res) {
-  let response = MOVIEAPI.movies;
+  let response = MOVIEAPI;
   if (req.query.genre) {
     reponse = response.filter(movies =>
-      movies.genre.includes(req.query.genre)
+      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     )
   }
   if (req.query.country) {
-    response = response.filter(pokemon =>
-      pokemon.country.includes(req.query.country)
+    response = response.filter(movies =>
+      movies.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+  if (req.query.avg_vote) {
+    response = response.filter(movies =>
+      Number(movies.avg_vote) >= Number(req.query.avg_vote)
     )
   }
   res.json(response)
 })
 
-const PORT = 8000
+const PORT = 9000
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
